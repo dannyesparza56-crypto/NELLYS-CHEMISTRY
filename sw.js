16ec3b38002b62405ddfed982087f2b788040670
dannyesparza56-crypto <dannyesparza56@gmail.com>
2026-03-09 16:27:48 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
-const CACHE_NAME = 'chem-lab-v2';
+const CACHE_NAME = 'chem-v50';
 const ASSETS = ['./', './index.html', './style.css'];
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((ks) => {
+    return Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+  }));
 });
 
 self.addEventListener('fetch', (e) => {
