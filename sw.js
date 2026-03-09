44056629f8eadbf6788c3c6d85b5afcb8dd1e68e
dannyesparza56-crypto <dannyesparza56@gmail.com>
2026-03-09 16:08:16 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'chem-lab-v1';
+const ASSETS = ['./', './index.html', './style.css'];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
